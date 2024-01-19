49b12596ef2454f01a43998ad82d46760cb9d650
Michael Shyu <michaelshyu11@gmail.com>
2024-01-19 15:46:35 -0500

    compltered lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(arr, arg) {
+    let newArr = arr.filter(function (element) {
+        return element.toLowerCase() === arg.toLowerCase();
+    })
+    return newArr;
+}
+
+function fuzzyMatch(arr, arg) {
+    let newArr = arr.filter(function (element) {
+        return element.toLowerCase().substring(0, arg.length) == arg.toLowerCase();
+    })
+    return newArr;
+}
+
+function matchName(arr, arg) {
+    let newArr = arr.filter(function (element) {
+        return element.name === arg;
+    })
+    return newArr;
+}
